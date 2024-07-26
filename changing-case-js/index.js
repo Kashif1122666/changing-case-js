5dbfb71621feb3874d489c45fbd8ba1b7bdfeded
Kashif1122666 <158280129+Kashif1122666@users.noreply.github.com>
2024-07-26 14:37:10 -0700

    Add files via upload
    changing-case-js

---
+++ changing-case-js/index.js
@@ -0,0 +1,72 @@
+// Chapter-21: CHANGING CASE
+
+// lowerCase: helloworld.
+
+// upperCAse:HELLOWORLD.
+
+// toLowerCase()
+// const city = "peshawar"
+// console.log(city);
+
+// using second variable
+// var city = "PESHAWAR"
+// const lowerCase = city.toLowerCase();
+// console.log(city);
+// console.log(lowerCase);
+
+// without using second variable
+// console.log(city.toLowerCase());
+    // city = city.toLowerCase();
+    // console.log(city);
+
+    // not imporatant to have all in uppercase we can get mix of upper and lower
+    // const city = "KaRachi"
+    // console.log(city.toLowerCase());
+
+    // toUpperCase();
+    // var city = "peshawar"
+
+    // using second variable 
+    // const capitalizedCity = city.toUpperCase();
+    // console.log(city);
+    // console.log(capitalizedCity);
+
+    // without using second variable
+    // console.log(city.toUpperCase());
+    // city = city.toUpperCase();
+    // console.log(city);
+
+    // not imporatant to have all in lowercase we can get mix of upper and lower
+    // const city = "pesHaWaR"
+    // console.log(city.toUpperCase());
+
+    // Real world example
+    // Laptopscheme
+    // const eligiableUniversities = [
+    //     "sarhad",
+    //     "iqra",
+    //     "islamia",
+    //     "city",
+    //     "ims",
+    //     "cecose",
+    //     "nust",
+    //     "agriculture"
+    // ];
+    // const studentUniversity = prompt("please enter your university name");
+    // let isEligiable = false;
+
+    // for (let universityindex = 0;
+    //      universityindex<eligiableUniversities.length; 
+    //     universityindex++){
+    //         if(studentUniversity.toLowerCase() === eligiableUniversities[universityindex]){
+    //             isEligiable = true;
+    //             alert("Eligiable");
+    //             break;
+    //         }
+    // }
+    // if (!isEligiable){
+    //    console.log("not eligiable");
+    // }
+
+
+
